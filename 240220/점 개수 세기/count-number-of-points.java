b28b9255597b18e81f05c48bfc3e19fc4025e495
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-02-20 10:19:52 +0900

    Title: 점 개수 세기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240220/점 개수 세기/count-number-of-points.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    private static int n;
+    private static int q;
+    private static int[] nums;
+    private static Queue<int[]> que = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        init();
+        findAnswer();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(input.readLine());
+        n = Integer.parseInt(tokenizer.nextToken());
+        q = Integer.parseInt(tokenizer.nextToken());
+
+        nums = new int[n];
+        tokenizer = new StringTokenizer(input.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+
+        for (int i = 0; i < q; i++) {
+            tokenizer = new StringTokenizer(input.readLine());
+            int start = Integer.parseInt(tokenizer.nextToken());
+            int end = Integer.parseInt(tokenizer.nextToken());
+            que.offer(new int[]{start, end});
+        }
+    }
+
+    private static void findAnswer() {
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(nums[i]);
+        }
+
+        int cnt = 1;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (Integer num : set) {
+            map.put(num, cnt);
+            cnt++;
+        }
+
+        while (!que.isEmpty()) {
+            int[] range = que.poll();
+            int start = map.ceilingKey(range[0]);
+            int end = map.floorKey(range[1]);
+            System.out.println(map.get(end) - map.get(start) + 1);
+        }
+    }
+}
