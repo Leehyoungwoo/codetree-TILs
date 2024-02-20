0e7a1bc247491d1fc70bbc93510658114ca64845
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-02-20 09:57:03 +0900

    Title: 점 개수 세기 3, Time: 1772ms, Memory: 54MB, Status: Passed - CodeTree

--- 240220/점 개수 세기 3/count-number-of-points-3.java
+++ 240220/점 개수 세기 3/count-number-of-points-3.java
@@ -1,26 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int q = input.nextInt();
 
-        int[] points = new int[n];
+    private static int n;
+    private static int q;
+    private static int[] nums;
+    private static Queue<int[]> que = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        init();
+        findAnswer();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(input.readLine());
+        n = Integer.parseInt(tokenizer.nextToken());
+        q = Integer.parseInt(tokenizer.nextToken());
+
+        nums = new int[n];
+        tokenizer = new StringTokenizer(input.readLine());
         for (int i = 0; i < n; i++) {
-            points[i] = input.nextInt();
+            nums[i] = Integer.parseInt(tokenizer.nextToken());
         }
 
         for (int i = 0; i < q; i++) {
-            int start = input.nextInt();
-            int end = input.nextInt();
-            int cnt = 0;
-            for (int j = 0; j < n; j++) {
-                if ( start <=points[j] && points[j] <= end) {
-                    cnt++;
-                }
-            }
-            System.out.println(cnt);
+            tokenizer = new StringTokenizer(input.readLine());
+            int start = Integer.parseInt(tokenizer.nextToken());
+            int end = Integer.parseInt(tokenizer.nextToken());
+            que.offer(new int[]{start, end});
+        }
+    }
+
+    private static void findAnswer() {
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        for (int i = 0; i < n; i++) {
+            treeSet.add(nums[i]);
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int cnt = 1;
+        for (Integer num : treeSet) {
+            map.put(num, cnt);
+            cnt++;
+        }
+
+        while (!que.isEmpty()) {
+            int[] q = que.poll();
+            int start = q[0];
+            int end = q[1];
+            System.out.println(map.get(end) - map.get(start) + 1);
         }
     }
 }
