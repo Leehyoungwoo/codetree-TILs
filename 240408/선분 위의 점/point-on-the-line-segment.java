926b57e672b0d56d653b42a9e7ba77cc01d10256
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-04-08 23:04:16 +0900

    Title: 선분 위의 점, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240408/선분 위의 점/point-on-the-line-segment.java
+++ 240408/선분 위의 점/point-on-the-line-segment.java
@@ -1,72 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Main {
+    public static final int MAX_N = 100000;
 
-    private static final int RANGE = 1_000_000_000;
-    private static int n;
-    private static int m;
-    private static TreeMap<Integer, Integer> map = new TreeMap<>();
-    private static Queue<int[]> que = new LinkedList<>();
+    public static int n, m;
+    public static TreeMap<Integer, Integer> map = new TreeMap<>();
 
-    public static void main(String[] args) throws IOException {
-        StringBuilder answer = new StringBuilder();
-        init();
-        for (int i = 0; i < m; i++) {
-            int[] poll = que.poll();
-            int left = poll[0];
-            int right = poll[1];
-            int num = findAnswer(left, right);
-            answer.append(num).append("\n");
+    // target보다 큰 최초의 위치를 반환합니다.
+    public static int upperBound(int target) {
+        Integer key = map.ceilingKey(target);
+        if (key == null) {
+            return n;
         }
-
-        System.out.println(answer);
+        return map.get(key);
     }
 
-    private static int findAnswer(int left, int right) {
-        int first = 0;
-        int last = 0;
-
-        if (right < map.firstKey()) {
+    // target보다 같거나 큰 최초의 위치를 반환합니다.
+    public static int lowerBound(int target) {
+        Integer key = map.floorKey(target);
+        if (key == null) {
             return 0;
         }
-
-        if (left > map.lastKey()) {
-            return 0;
-        }
-
-        if (map.ceilingEntry(left) != null) {
-            first = map.ceilingEntry(left).getValue();
-        }
-
-        if (map.floorEntry(right) != null) {
-            last = map.floorEntry(right).getValue();
-        }
-
-        return (last - first + 1);
+        return map.get(key);
     }
 
-    private static void init() throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(input.readLine());
-        n = Integer.parseInt(tokenizer.nextToken());
-        m = Integer.parseInt(tokenizer.nextToken());
-        int[] arr = new int[n];
-        tokenizer = new StringTokenizer(input.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 입력
+        n = sc.nextInt();
+        m = sc.nextInt();
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(tokenizer.nextToken());
+            int num = sc.nextInt();
+            map.put(num, i);
         }
-        Arrays.sort(arr);
-        for (int i = 0; i < n; i++) {
-            map.put(arr[i], i + 1);
-        }
+
+        // m개의 질의에 대한 답을 계산합니다.
         for (int i = 0; i < m; i++) {
-            tokenizer = new StringTokenizer(input.readLine());
-            int start = Integer.parseInt(tokenizer.nextToken());
-            int end = Integer.parseInt(tokenizer.nextToken());
-            que.offer(new int[]{start, end});
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            // b보다 큰 최초의 숫자 위치에서
+            // a보다 같거나 큰 최초의 숫자 위치를 빼면
+            // 문제에서 원하는 답이 됩니다.
+            int count = upperBound(b) - lowerBound(a);
+            System.out.println(count);
         }
     }
 }
