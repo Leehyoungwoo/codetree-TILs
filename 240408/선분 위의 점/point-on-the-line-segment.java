8f68111dea080a700259320a9d5416c12c34e30a
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-04-08 23:16:38 +0900

    Title: 선분 위의 점, Time: 489ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240408/선분 위의 점/point-on-the-line-segment.java
+++ 240408/선분 위의 점/point-on-the-line-segment.java
@@ -1,66 +1,51 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.TreeMap;
 
 public class Main {
     public static final int MAX_N = 100000;
-    
+
     public static int n, m;
-    public static int[] arr = new int[MAX_N];
-    
+    public static TreeMap<Integer, Integer> map = new TreeMap<>();
+
+    // target보다 크거나 같은 최초의 위치를 반환합니다.
+    public static int lowerBound(int target) {
+        Integer key = map.ceilingKey(target);
+        if (key == null) {
+            return n;
+        }
+        return map.get(key);
+    }
+
     // target보다 큰 최초의 위치를 반환합니다.
     public static int upperBound(int target) {
-        int left = 0, right = n - 1;
-        int minIdx = n;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if(arr[mid] > target) {
-                right = mid - 1;
-                minIdx = Math.min(minIdx, mid);
-            }
-            else
-                left = mid + 1;
+        Integer key = map.higherKey(target);
+        if (key == null) {
+            return n;
         }
-    
-        return minIdx;
-    }
-    
-    // target보다 같거나 큰 최초의 위치를 반환합니다.
-    public static int lowerBound(int target) {
-        int left = 0, right = n - 1;
-        int minIdx = n;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if(arr[mid] >= target) {
-                right = mid - 1;
-                minIdx = Math.min(minIdx, mid);
-            }
-            else
-                left = mid + 1;
-        }
-    
-        return minIdx;
+        return map.get(key);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         // 입력
         n = sc.nextInt();
         m = sc.nextInt();
-        for(int i = 0; i < n; i++)
-            arr[i] = sc.nextInt();
 
-        // 이진탐색을 진행하기 전에
-        // 정렬을 진행해줍니다.
-        Arrays.sort(arr, 0, n);
+        // 점의 좌표를 TreeMap에 저장
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            map.put(num, i);
+        }
 
         // m개의 질의에 대한 답을 계산합니다.
-        for(int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
 
             // 이진탐색을 진행합니다.
             // b보다 큰 최초의 숫자 위치에서
-            // a보다 같거나 큰 최초의 숫자 위치를 빼면
+            // a보다 크거나 같은 최초의 숫자 위치를 빼면
             // 문제에서 원하는 답이 됩니다.
             int count = upperBound(b) - lowerBound(a);
             System.out.println(count);
