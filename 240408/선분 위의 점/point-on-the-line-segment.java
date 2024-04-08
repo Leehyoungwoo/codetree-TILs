a6927f72a4eb9ae8500c59d92ec1052ac2af0615
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-04-08 23:19:14 +0900

    Title: 선분 위의 점, Time: 2730ms, Memory: 81MB, Status: Wrong Answer - CodeTree

--- 240408/선분 위의 점/point-on-the-line-segment.java
+++ 240408/선분 위의 점/point-on-the-line-segment.java
@@ -1,13 +1,12 @@
 import java.util.*;
 
-
 public class Main {
     public static final int MAX_N = 100000;
 
     public static int n, m;
     public static TreeMap<Integer, Integer> map = new TreeMap<>();
 
-    // target보다 크거나 같은 최초의 위치를 반환합니다.
+    // target보다 크거나 같은 최초의 위치의 인덱스를 반환합니다.
     public static int lowerBound(int target) {
         Integer key = map.ceilingKey(target);
         if (key == null) {
@@ -16,7 +15,7 @@ public class Main {
         return map.get(key);
     }
 
-    // target보다 큰 최초의 위치를 반환합니다.
+    // target보다 큰 최초의 위치의 인덱스를 반환합니다.
     public static int upperBound(int target) {
         Integer key = map.higherKey(target);
         if (key == null) {
@@ -42,15 +41,13 @@ public class Main {
             map.put(arr[i], i);
         }
 
-
         // m개의 질의에 대한 답을 계산합니다.
         for (int i = 0; i < m; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
 
-            // 이진탐색을 진행합니다.
             // b보다 큰 최초의 숫자 위치에서
-            // a보다 크거나 같은 최초의 숫자 위치를 빼면
+            // a보다 크거나 같은 최초의 숫자 위치의 인덱스를 빼면
             // 문제에서 원하는 답이 됩니다.
             int count = upperBound(b) - lowerBound(a);
             System.out.println(count);
