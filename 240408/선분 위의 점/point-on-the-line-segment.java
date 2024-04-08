199e2f0dcebef34317dd9ffbf3899f781d69e6c5
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-04-08 23:12:52 +0900

    Title: 선분 위의 점, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240408/선분 위의 점/point-on-the-line-segment.java
+++ 240408/선분 위의 점/point-on-the-line-segment.java
@@ -1,76 +1,69 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
-
-    private static final int RANGE = 1_000_000_000;
-    private static int n;
-    private static int m;
-    private static TreeMap<Integer, Integer> map = new TreeMap<>();
-    private static Queue<int[]> que = new LinkedList<>();
-
-    public static void main(String[] args) throws IOException {
-        StringBuilder answer = new StringBuilder();
-        init();
-        for (int i = 0; i < m; i++) {
-            int[] poll = que.poll();
-            int left = poll[0];
-            int right = poll[1];
-            int num = findAnswer(left, right);
-            answer.append(num).append("\n");
+    public static final int MAX_N = 100000;
+    
+    public static int n, m;
+    public static int[] arr = new int[MAX_N];
+    
+    // target보다 큰 최초의 위치를 반환합니다.
+    public static int upperBound(int target) {
+        int left = 0, right = n - 1;
+        int minIdx = n;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if(arr[mid] > target) {
+                right = mid - 1;
+                minIdx = Math.min(minIdx, mid);
+            }
+            else
+                left = mid + 1;
         }
-
-        System.out.println(answer);
+    
+        return minIdx;
+    }
+    
+    // target보다 같거나 큰 최초의 위치를 반환합니다.
+    public static int lowerBound(int target) {
+        int left = 0, right = n - 1;
+        int minIdx = n;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if(arr[mid] >= target) {
+                right = mid - 1;
+                minIdx = Math.min(minIdx, mid);
+            }
+            else
+                left = mid + 1;
+        }
+    
+        return minIdx;
     }
 
-    private static int findAnswer(int left, int right) {
-        int first = 0;
-        int last = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
 
-        if (right < map.firstKey()) {
-            return 0;
-        }
+        // 이진탐색을 진행하기 전에
+        // 정렬을 진행해줍니다.
+        Arrays.sort(arr, 0, n);
 
-        if (left > map.lastKey()) {
-            return 0;
-        }
+        // m개의 질의에 대한 답을 계산합니다.
+        for(int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
 
-        if (map.ceilingEntry(left) != null) {
-            first = map.ceilingEntry(left).getValue();
-        } else {
-            first = 0;
-        }
-
-        if (map.floorEntry(right) != null) {
-            last = map.floorEntry(right).getValue();
-        } else {
-            last = n;
-        }
-
-        return (last - first + 1);
-    }
-
-    private static void init() throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(input.readLine());
-        n = Integer.parseInt(tokenizer.nextToken());
-        m = Integer.parseInt(tokenizer.nextToken());
-        int[] arr = new int[n];
-        tokenizer = new StringTokenizer(input.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(tokenizer.nextToken());
-        }
-        Arrays.sort(arr);
-        for (int i = 0; i < n; i++) {
-            map.put(arr[i], i + 1);
-        }
-        for (int i = 0; i < m; i++) {
-            tokenizer = new StringTokenizer(input.readLine());
-            int start = Integer.parseInt(tokenizer.nextToken());
-            int end = Integer.parseInt(tokenizer.nextToken());
-            que.offer(new int[]{start, end});
+            // 이진탐색을 진행합니다.
+            // b보다 큰 최초의 숫자 위치에서
+            // a보다 같거나 큰 최초의 숫자 위치를 빼면
+            // 문제에서 원하는 답이 됩니다.
+            int count = upperBound(b) - upperBound(a);
+            System.out.println(count);
         }
     }
 }
